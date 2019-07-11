3c0b8cb46fda790ad06b69ec134c0f7f925aa73c
sg <srfngrc@outlook.com>
2019-07-11 18:28:56 -0400

    6h30pm

--- Program.cs
+++ Program.cs
@@ -14,21 +14,38 @@ namespace Ch09Exercices
     }
     public interface IMyBaseInterface
     {
+        string DoSomething();
     }
     internal interface IMyBaseInterface2
     {
+        int DoSomething2();
     }
     internal interface IMyInterface : IMyBaseInterface, IMyBaseInterface2
     {
+        void DoSomething3();
     }
     internal sealed class MyComplexClass : MyClass, IMyInterface
     {
+        public string DoSomething()
+        {
+            throw new NotImplementedException();
+        }
+        public int DoSomething2()
+        {
+            throw new NotImplementedException();
+        }
+        public void DoSomething3()
+        {
+            throw new NotImplementedException();
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
+            //MyBase myBase = new MyBase();
             MyComplexClass myObj = new MyComplexClass();
+            //myObj.
             Console.WriteLine(myObj.ToString());
             Console.ReadKey();
         }
